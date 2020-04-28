eade49f49d8ec00cea397241236e0194e6c3523c
swlee8783 <siwon8783@gmail.com>
2020-04-28 20:57:38 +0900

    2차 요구사항 test 파일 수정

--- src/test/java/util/HttpResponseTest.java
+++ src/test/java/util/HttpResponseTest.java
@@ -4,34 +4,36 @@ import static org.junit.Assert.assertEquals;
 
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.io.FIleOutputStream;
+import java.io.FileOutputStream;
 import java.io.OutputStream;
 
 import org.junit.Test;
+
+import http.HttpResponse;
 
 public class HttpResponseTest {
 	private String testDirectory = "./src/test/resources/";
 	
 	@Test
-	public void responseForward() throws Exception() {
+	public void responseForward() throws Exception {
 		HttpResponse response = new HttpResponse(createOutputStream("Http_Forward.txt"));
 		response.forward("/index.html");
 	}
 	
 	@Test
-	public void responseRedirect() throws Exception() {
+	public void responseRedirect() throws Exception {
 		HttpResponse response = new HttpResponse(createOutputStream("Http_Redirect.txt"));
 		response.sendRedirect("/index.html");
 	}
 	
 	@Test
-	public void responseCookies() throws Exception() {
+	public void responseCookies() throws Exception {
 		HttpResponse response = new HttpResponse(createOutputStream("Http_Cookie.txt"));
 		response.addHeader("Set-Cookie", "logined=true");
 		response.sendRedirect("/index.html");
 	}
 	
 	private OutputStream createOutputStream(String filename) throws FileNotFoundException {
-		return new FIleOutputStream(new File(testDirectory + filename));
+		return new FileOutputStream(new File(testDirectory + filename));
 	}
 }
