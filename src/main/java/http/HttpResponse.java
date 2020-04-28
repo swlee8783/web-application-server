fd457894e961801af473e99f6509a83adee877e8
swlee8783 <siwon8783@gmail.com>
2020-04-29 00:47:21 +0900

    요구사항 1 50%, 요구사항 2 test 작성

--- src/main/java/http/HttpResponse.java
+++ src/main/java/http/HttpResponse.java
@@ -12,17 +12,17 @@ import org.slf4j.LoggerFactory;
 
 import util.HttpRequestUtils;
 import util.IOUtils;
+import http.RequestLine;
 
-public class HttpRequest {
-	private static final Logger log = LoggerFactory.getLogger(HttpRequest.class);
+public class HttpResponse {
+	private static final Logger log = LoggerFactory.getLogger(HttpResponse.class);
 	
 	private Map<String, String> headers = new HashMap<String, String>();
 	private Map<String, String> params = new HashMap<String, String>();
 	private String method;
-	private String path;
 	private RequestLine requestLine;
 	
-	public HttpRequest(InputStream in) {
+	public HttpResponse(InputStream in) {
 		try {
 			BufferedReader br = new BufferedReader(new InputStreamReader(in, "UTF-8"));
 			String line = br.readLine();
@@ -31,13 +31,15 @@ public class HttpRequest {
 				return;
 			}
 			
-			RequestLine(line);
+			requestLine = new RequestLine(line);
 			
 			line = br.readLine();
 			
-			while(!"".equals(line)) {
+			while(!line.equals("")){
 				log.debug("Header: {}", line);
 				String[] tokens = line.split(":");
+				log.debug(tokens[0]);
+				log.debug(tokens[1]);
 				headers.put(tokens[0].trim(), tokens[1].trim());
 				line = br.readLine();
 			}
@@ -45,52 +47,20 @@ public class HttpRequest {
 			if ("POST".equals(method)) {
 				String query = IOUtils.readData(br, Integer.parseInt(headers.get("Content-Length")));
 				params = HttpRequestUtils.parseQueryString(query);
+			} else {
+				params = requestLine.getParams();
 			}
-		} catch(IOException e) {
+		}
+		catch(IOException e) {
 			log.error(e.getMessage());
 		}
 	}
 	
-	private void RequestLine(String requestLine) {
-		log.debug("Request line: {}", requestLine);
-		String [] tokens = requestLine.split(" ");
-		method = tokens[0];
-		
-		if ("POST".equals(method)) {
-			path = tokens[1];
-			log.debug("PATH: {}", path);
-			return;
-		}
-		
-		Integer index = tokens[1].indexOf("?");
-		
-		if (index == -1) {
-			path = tokens[1];
-			log.debug("PATH: {}", path);
-		} else {
-			path = tokens[1].substring(0, index);
-			log.debug("PATH: {}", path);
-			params = HttpRequestUtils.parseQueryString(tokens[1].substring(index + 1));
-		}	
-	}
-	
-	public String getMethod() {
-		log.debug("getMethod_Answer: {}",method);
-		return method;
-	}
-	
-	public String getPath() {
-		log.debug("getPath_Answer: {}",path);
-		return path;
-	}
-	
 	public String getHeader(String field_name) {
-		log.debug("getHeader_Answer: {}",headers.get(field_name));
 		return headers.get(field_name);
 	}
 	
 	public String getParameter(String param_name) {
-		log.debug("getParameter_Answer: {}",params.get(param_name));
 		return params.get(param_name);
 	}
 }
